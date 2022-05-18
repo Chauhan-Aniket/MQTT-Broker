1c8e8035a833bf0d8e4337279b53ab397876dcfb
Chauhan-Aniket <aniketchauhan2125@gmail.com>
2022-05-18 19:37:26 +0530

    modified script for aedes server

--- index.js
+++ index.js
@@ -1,34 +1,7 @@
-// MQTT broker
-var mosca = require("mosca");
+const aedes = require("aedes")();
+const server = require("net").createServer(aedes.handle);
+const port = 1883;
 
-var pubsubsettings = {
-	//using ascoltatore
-	type: "mongo",
-	url: "mongodb://localhost:27017/mqtt",
-	pubsubCollection: "ascoltatori",
-	mongo: {},
-};
-
-var settings = {
-	port: process.env.PORT || 1883, //mosca (mqtt) port
-	host: "my-mqttbroker.herokuapp.com" || "127.0.0.1",
-	backend: pubsubsettings, //pubsubsettings is the object we created above
-};
-
-var server = new mosca.Server(settings);
-
-server.on("clientConnected", function (client) {
-	console.log("client connected", client.id);
+server.listen(port, function () {
+	console.log("server started and listening on port ", port);
 });
-
-// fired when a message is received
-server.on("published", function (packet, client) {
-	console.log("Published", packet.payload);
-});
-
-server.on("ready", setup);
-
-// fired when the mqtt server is ready
-function setup() {
-	console.log("Mosca server is up and running");
-}
