e77056e43ff057ff72713688f280c5cabe81c488
Chauhan-Aniket <aniketchauhan2125@gmail.com>
2022-05-18 16:42:21 +0530

    modified pubsub-setting

--- index.js
+++ index.js
@@ -1,13 +1,33 @@
 // MQTT broker
 var mosca = require("mosca");
-var settings = { port: process.env.PORT || 1883 };
-var broker = new mosca.Server(settings);
 
-broker.on("ready", () => {
-	console.log("Broker is ready!");
+var pubsubsettings = {
+	type: "mqtt",
+	json: false,
+	mqtt: require("mqtt"),
+	host: "my-mqttbroker.herokuapp.com" || "127.0.0.1",
+	port: process.env.PORT || 1883,
+};
+
+var settings = {
+	port: 1883,
+	backend: pubsubsettings,
+};
+
+var server = new mosca.Server(settings);
+
+server.on("clientConnected", function (client) {
+	console.log("client connected", client.id);
 });
 
-broker.on("published", (packet) => {
-	message = packet.payload.toString();
-	console.log(message);
+// fired when a message is received
+server.on("published", function (packet, client) {
+	console.log("Published", packet.payload);
 });
+
+server.on("ready", setup);
+
+// fired when the mqtt server is ready
+function setup() {
+	console.log("Mosca server is up and running");
+}
