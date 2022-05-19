ecd7a468f77d7f774dac57d2bb4f9f2fbe1a9a11
Chauhan-Aniket <aniketchauhan2125@gmail.com>
2022-05-19 15:15:55 +0530

    added ws server

--- index.js
+++ index.js
@@ -6,21 +6,15 @@ var app = express();
 app.use(favicon(path.join(__dirname, "public", "favicon.ico")));
 
 const aedes = require("aedes")();
-const server = require("net").createServer(aedes.handle);
-// const httpServer = require("http").createServer();
-// const ws = require("websocket-stream");
+const httpServer = require("http").createServer();
+const ws = require("websocket-stream");
 const port = process.env.PORT || 1234;
-// const wsPort = process.env.WSPORT || 80;
 
-server.listen(port, function () {
-	console.log("server started and listening on port ", port);
+ws.createServer({ server: httpServer }, aedes.handle);
+
+httpServer.listen(port, function () {
+	console.log("websocket server listening on port ", port);
 });
-
-// ws.createServer({ server: httpServer }, aedes.handle);
-
-// httpServer.listen(wsPort, function () {
-// 	console.log("websocket server listening on port ", wsPort);
-// });
 
 aedes.on("publish", (packet, client) => {
 	if (client) {
