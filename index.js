1630b321324b6a7182169ff366d9f5f6993bd988
Chauhan-Aniket <aniketchauhan2125@gmail.com>
2022-05-19 10:37:50 +0530

    modified broker

--- index.js
+++ index.js
@@ -1,12 +1,7 @@
-require("dotenv").config();
-
 const aedes = require("aedes")();
-const httpServer = require("http").createServer();
-const ws = require("websocket-stream");
-const port = process.env.PORT || 1883;
+const server = require("net").createServer(aedes.handle);
+const port = process.env.PORT || 1234;
 
-ws.createServer({ server: httpServer }, aedes.handle);
-
-httpServer.listen(port, function () {
-	console.log("websocket server listening on port ", port);
+server.listen(port, function () {
+	console.log("server started and listening on port ", port);
 });
