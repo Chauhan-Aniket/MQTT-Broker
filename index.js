912ecd40a1b86331a0e8e2c22b3c3ae9c64c11f1
Chauhan-Aniket <aniketchauhan2125@gmail.com>
2022-05-19 12:32:53 +0530

    updated to websocket broker

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const aedes = require("aedes")();
-const server = require("net").createServer(aedes.handle);
-const port = process.env.PORT || 1234;
+const httpServer = require("http").createServer();
+const ws = require("websocket-stream");
+const port = process.env.PORT || 80;
 
-server.listen(port, function () {
-	console.log("server started and listening on port ", port);
+ws.createServer({ server: httpServer }, aedes.handle);
+
+httpServer.listen(port, () => {
+	console.log("websocket server listening on port ", port);
 });
