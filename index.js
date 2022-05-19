0658285efec029b3cfa210102ad61cf5188ab1cf
Chauhan-Aniket <aniketchauhan2125@gmail.com>
2022-05-19 10:08:04 +0530

    modified broker

--- index.js
+++ index.js
@@ -1,35 +1,12 @@
-// MQTT broker
-var mosca = require("mosca");
 require("dotenv").config();
 
-var pubsubsettings = {
-	//using ascoltatore
-	type: "mongo",
-	url: `mongodb://${process.env.MONGO_USER}:${process.env.MONGO_PWD}@${process.env.APP_URL}:27017/${process.env.CLUSTER_NAME}`,
-	pubsubCollection: "ascoltatori",
-	mongo: {},
-};
+const aedes = require("aedes")();
+const httpServer = require("http").createServer();
+const ws = require("websocket-stream");
+const port = process.env.PORT || 1883;
 
-var settings = {
-	port: process.env.PORT || 1883, //mosca (mqtt) port
-	host: "my-mqttbroker.herokuapp.com" || "127.0.0.1",
-	backend: pubsubsettings, //pubsubsettings is the object we created above
-};
+ws.createServer({ server: httpServer }, aedes.handle);
 
-var server = new mosca.Server(settings);
-
-server.on("clientConnected", function (client) {
-	console.log("client connected", client.id);
+httpServer.listen(port, function () {
+	console.log("websocket server listening on port ", port);
 });
-
-// fired when a message is received
-server.on("published", function (packet, client) {
-	console.log("Published", packet.payload);
-});
-
-server.on("ready", setup);
-
-// fired when the mqtt server is ready
-function setup() {
-	console.log("Mosca server is up and running");
-}
